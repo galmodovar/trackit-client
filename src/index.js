a8506d3a62b18c15b17e4d6efbd0f480a685f90d
galmodovar <galmodovar1@gmail.com>
2021-12-06 10:34:04 -0600

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { TrackIt } from './components/TrackIt';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+    <TrackIt />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
