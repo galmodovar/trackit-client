17d6b4bbe1cb82e313e494142e94d0788157d6fc
galmodovar <galmodovar1@gmail.com>
2021-12-15 11:06:16 -0600

    deploy

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -26,7 +26,7 @@ export const Register = () => {
                 "password": password.current.value
             }
 
-            return fetch("http://127.0.0.1:8000/register", {
+            return fetch("https://trackit-server-side.herokuapp.com/register", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
