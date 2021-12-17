4aa0db675f807fdd2aa803970316da3f5e6328c1
galmodovar <galmodovar1@gmail.com>
2021-12-17 13:27:15 -0600

    drag and drop working

--- src/components/application/ApplicationCard.js
+++ src/components/application/ApplicationCard.js
@@ -1,38 +1,94 @@
-import { useEffect, useState } from "react"
-import { getApplications, deleteApp, getSearchedApps } from "./ApplicationManager.js"
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
+import React, { useState, useEffect } from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import { getApplications, getSearchedApps, deleteApp } from "./ApplicationManager";
 import Button from '@mui/material/Button';
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 import DeleteForeverOutlinedIcon from '@mui/icons-material/DeleteForeverOutlined';
 import MoreHorizOutlinedIcon from '@mui/icons-material/MoreHorizOutlined';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
-import "./Application.css"
 
 
+export const ApplicationCard =() => {
+const [applications, setApplications] = useState([])
+const [query, setQuery] = useState([])
+const [columns, setColumns] = useState({});
+const history = useHistory()
 
-export const ApplicationCard = () => {
-    const [ applications, setApplications ] = useState([])
-    const [query, setQuery] = useState('')
-    const history = useHistory()
-
-    const fetchApplications = () => {
-        getApplications().then(data => setApplications(data))
-    }
-
-    useEffect(() => {
-        if (query) {
-            getSearchedApps(query).then(data => setApplications(data))
-        } else {
-            fetchApplications()
+useEffect(() => {
+    setColumns({
+        [1]: {
+          name: "Researching",
+          items: applications
+        },
+        [2]: {
+          name: "Applied",
+          items: []
+        },
+        [3]: {
+          name: "Finished",
+          items: []
         }
-    }, [query])
+      })
+}, [applications])
 
-    return (
-        <>
-        <form>
+
+
+const fetchApplications = () => {
+    getApplications().then(data => setApplications(data))
+}
+
+useEffect(() => {
+    if (query) {
+        getSearchedApps(query).then(data => setApplications(data))
+    } else {
+        fetchApplications()
+    }
+}, [query])
+
+
+
+
+const onDragEnd = (result, columns, setColumns) => {
+  if (!result.destination) return;
+  const { source, destination } = result;
+
+  if (source.droppableId !== destination.droppableId) {
+    const sourceColumn = columns[source.droppableId];
+    const destColumn = columns[destination.droppableId];
+    const sourceItems = [...sourceColumn.items];
+    const destItems = [...destColumn.items];
+    const [removed] = sourceItems.splice(source.index, 1);
+    destItems.splice(destination.index, 0, removed);
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...sourceColumn,
+        items: sourceItems
+      },
+      [destination.droppableId]: {
+        ...destColumn,
+        items: destItems
+      }
+    });
+  } else {
+    const column = columns[source.droppableId];
+    const copiedItems = [...column.items];
+    const [removed] = copiedItems.splice(source.index, 1);
+    copiedItems.splice(destination.index, 0, removed);
+    setColumns({
+      ...columns,
+      [source.droppableId]: {
+        ...column,
+        items: copiedItems
+      }
+    });
+  }
+};
+
+  
+  return (
+      <>
+      <form>
             <input
                 type="text"
                 id="header-search"
@@ -42,55 +98,98 @@ export const ApplicationCard = () => {
                     />
                 
             </form>
-        
-        
-        <section className="app-board">
-        <article className="application">
-            <h1>Researching</h1>
-            {
-                applications?.map(application => {
-                    
+    <div style={{ display: "flex", justifyContent: "center", height: "100%" }}>
+      <DragDropContext
+        onDragEnd={result => onDragEnd(result, columns, setColumns)}
+      >
+        {Object.entries(columns)?.map(([columnId, column], index) => {
+          return (
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center"
+              }}
+              key={columnId}
+            >
+              <h2>{column.name}</h2>
+              <div style={{ margin: 8 }}>
+                <Droppable droppableId={columnId} key={columnId}>
+                  {(provided, snapshot) => {
                     return (
-                      <Card sx={{ minWidth: 275 }}>
-                        <CardContent style={{background: '#00897b'}}>
-                            <div className="application__role">{application.job_post.role} by {application.job_post.company}</div>
-                            <div className="application__date">Date Applied: {application.date_applied}</div>
-                            <div className="application__status">Currently in {application.status.status}</div>
-                        </CardContent>
-                        <CardActions style={{background: '#004d40'}} >
-                            <Button type="submit" size="small"
-                                onClick={() => {
-                                    history.push({ pathname: `/applications/${application.id}`})
-                                    }}><MoreHorizOutlinedIcon/></Button>
-                            <Button type="submit" size="small"
-                                onClick={() => {
-                                    history.push({ pathname: `/applications/edit/${application.id}`})
-                                    }}><EditOutlinedIcon/></Button>
-                            <Button type="submit" size="small"
-                                onClick={evt => {
+                      <div
+                        {...provided.droppableProps}
+                        ref={provided.innerRef}
+                        style={{
+                          background: snapshot.isDraggingOver
+                            ? "lightblue"
+                            : "lightgrey",
+                          padding: 4,
+                          width: 250,
+                          minHeight: 500
+                        }}
+                      >
+                        {column.items.map((item, index) => {
+                          return (
+                            <Draggable
+                                key={item.id}
+                                draggableId={(item.id).toString()}
+                                index={index}
+                            >
+                              {(provided, snapshot) => {
+                                return (
+                                  <div
+                                    ref={provided.innerRef}
+                                    {...provided.draggableProps}
+                                    {...provided.dragHandleProps}
+                                    style={{
+                                      userSelect: "none",
+                                      padding: 16,
+                                      margin: "0 0 8px 0",
+                                      minHeight: "50px",
+                                      backgroundColor: snapshot.isDragging
+                                        ? '#00897b'
+                                        : '#004d40',
+                                      color: "white",
+                                      ...provided.draggableProps.style
+                                    }}
+                                  >
+                                    <div className="application__role">{item.job_post.role} by {item.job_post.company}</div>
+                                    <div className="application__date">Date Applied: {item.date_applied}</div>
+                                    <Button type="submit" size="small"
+                                        onClick={() => {
+                                        history.push({ pathname: `/applications/${item.id}`})
+                                        }}><MoreHorizOutlinedIcon/></Button>
+                                    <Button type="submit" size="small"
+                                        onClick={() => {
+                                        history.push({ pathname: `/applications/edit/${item.id}`})
+                                        }}><EditOutlinedIcon/></Button>
+                                    <Button type="submit" size="small"
+                                    onClick={evt => {
                                     // Prevent form from being submitted
                                     evt.preventDefault()
-                                    deleteApp(application.id)
+                                    deleteApp(item.id)
                                     .then(() => fetchApplications())
-                                }}><DeleteForeverOutlinedIcon/></Button>
-                        </CardActions>
-                      </Card>
+                                    }}><DeleteForeverOutlinedIcon/></Button>
+                                  </div>
+                                );
+                              }}
+                            </Draggable>
+                          );
+                        })}
+                        {provided.placeholder}
+                      </div>
                     );
-                })
-            }
-        </article>
-        <article className="application">
-            <h1>Applied</h1>
-     
-        </article>
-        <article className="application">
-            <h1>Finished</h1>
-        
-        </article>
-        </section>
-
-        </>
-    )
+                  }}
+                </Droppable>
+              </div>
+            </div>
+          );
+        })}
+      </DragDropContext>
+    </div>
+    </>
+  );
 }
 
 
